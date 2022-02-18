85f1fff2dc2888aa46babe0c6b1df5e65867dad0
todelusion <todelusion.gmail.com>
2022-02-18 22:44:04 +0800

    2022.02.18_styling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-<DataBinding />
+  <DataBinding />
 </template>
 
 <script>
@@ -18,5 +18,10 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+  background-color: #f589be;
 }
 </style>
---
+++ src/components/DataBinding.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="mx-20 mt-10 box-border max-w-[768px] border-2 border-black bg-white px-20 py-24 font-bold lg:mx-auto"
+  >
+    <div class="text-lg text-black duration-150 hover:scale-105">
+      <label class="inline-block text-sm uppercase tracking-wider"
+        >email:</label
+      >
+      <input
+        type="text"
+        class="outline-none mt-1 box-border block w-full rounded-sm border-2 px-1 py-1 text-sm duration-150 hover:border-gray-500"
+      />
+    </div>
+  </div>
+</template>
