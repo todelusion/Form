143383ae4c3e244d7e8255a75910575f46f7b76f
todelusion <todelusion.gmail.com>
2022-02-18 21:12:52 +0800

    2022.02.18-DataBinding

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+<DataBinding />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import DataBinding from "./components/DataBinding.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    DataBinding,
   },
 };
 </script>
@@ -19,8 +18,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
