1d04245d5f7b6ecd19dfd9e7318522d47168fc8b
todelusion <todelusion.gmail.com>
2022-03-13 00:43:23 +0800

    change word

--- src/components/DataBinding.vue
+++ src/components/DataBinding.vue
@@ -1,11 +1,11 @@
 <template>
-  <form
-    class="mx-20 mt-36 box-border max-w-[768px] border-2 border-black bg-white px-20 py-24 font-bold duration-150 hover:scale-105 lg:mx-auto"
+  <div
+    class="mx-20 mt-36 box-border max-w-[768px] border-2 border-black bg-white px-20 py-24 font-bold lg:mx-auto"
   >
-    <!-- email -->
+    <!-- intrest-->
     <div class="text-lg text-black">
       <label class="inline-block text-sm uppercase tracking-wider"
-        >email:</label
+        >Interests</label
       >
       <p v-show="isSame" class="mt-2 inline text-center text-xs text-red-600">
         （無法輸入相同資料）
@@ -13,19 +13,19 @@
       <input
         type="text"
         class="outline-none mt-1 box-border block w-full rounded-sm border-2 px-1 py-1 text-sm duration-150 hover:border-gray-500"
-        v-model="tempEmail"
-        @keyup="inputEmail"
+        v-model="tempdata"
+        @keyup="inputdata"
       />
     </div>
-    <!-- password -->
-    <div class="mt-10 text-lg text-black">
-      <label class="inline-block text-sm uppercase tracking-wider"
-        >password:</label
+        <!-- display data -->
+    <div class="mt-3">
+      <div
+        v-for="data in datas"
+        :key="data"
+        class="mr-4 mt-4 inline-block cursor-pointer border-2 border-black px-3 hover:bg-black hover:text-white"
       >
-      <input
-        type="text"
-        class="outline-none mt-1 box-border block w-full rounded-sm border-2 px-1 py-1 text-sm duration-150 hover:border-gray-500"
-      />
+        <span @click="deleteItem(data)">{{ data }}</span>
+      </div>
     </div>
     <!-- career -->
     <div class="mt-10 text-lg text-black">
@@ -48,47 +48,33 @@
         >
       </div>
     </div>
-    <!-- display data -->
-    <div class="mt-16">
-      <h2
-        class="m-auto max-w-[30%] rounded-md border-2 border-black py-2 text-center text-lg uppercase text-black"
-      >
-        your data
-      </h2>
-      <div
-        v-for="email in emails"
-        :key="email"
-        class="mx-4 mt-4 inline-block cursor-pointer border-2 border-black px-3 hover:bg-black hover:text-white"
-      >
-        <span @click="deleteItem(email)">{{ email }}</span>
-      </div>
-    </div>
-  </form>
+
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      tempEmail: "",
-      emails: [],
+      tempdata: "",
+      datas: [],
       isSame: false,
     };
   },
   methods: {
-    inputEmail(e) {
-      if (e.key === "Enter" && this.tempEmail) {
-        if (!this.emails.includes(this.tempEmail)) {
+    inputdata(e) {
+      if (e.key === "Enter" && this.tempdata) {
+        if (!this.datas.includes(this.tempdata)) {
           this.isSame = false;
-          this.emails.push(this.tempEmail);
-          this.tempEmail = "";
+          this.datas.push(this.tempdata);
+          this.tempdata = "";
         } else {
           this.isSame = true;
         }
       }
     },
-    deleteItem(email) {
-      this.emails = this.emails.filter(item => email !== item);
+    deleteItem(data) {
+      this.datas = this.datas.filter(item => data !== item);
     },
   },
 };
